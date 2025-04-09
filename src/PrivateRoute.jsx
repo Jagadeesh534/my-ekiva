9f2a2b7a050e81051e7f0e6b1ca72871fb977582
Jagadeesh534 <kalugotlajagadeesh@gmail.com>
2025-04-09 22:45:33 +0530

    test

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = ({ children }) => {
-    debugger
   const isLoggedIn = useSelector((state) => state.auth.isAuthenticated); // or check token
 
   return isLoggedIn ? children : <Navigate to="/login" replace />;
